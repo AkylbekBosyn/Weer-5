cc30d4de9b48008329c08b2ff543dff0ff005d68
AkylbekBosyn <96645960+AkylbekBosyn@users.noreply.github.com>
2022-10-02 22:24:00 +0600

    Add files via upload

---
+++ week 5/task 2.py
@@ -0,0 +1,23 @@
+class Name:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+    def firstname(self):
+        return self.fname.title()
+    def lastname(self):
+        return self.lname.title()
+    def full_name(self):
+        return self.fname.title() + " " + self.lname.title()
+    def initials(self):
+        initials = str(self.fname[0]) + "." + str(self.lname[0])
+        return initials
+obj1 = Name('Akylbek', 'Bosyn')
+print(obj1.firstname())
+print(obj1.lastname())
+print(obj1.full_name())
+print(obj1.initials())
+obj2 = Name('Akylbek', 'Bosyn')
+print(obj2.firstname())
+print(obj2.lastname())
+print(obj2.full_name())
+print(obj2.initials())
